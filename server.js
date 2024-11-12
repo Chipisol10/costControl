8835245b48ed7fce6f3841eace374454e1f2b0c4
Luis G <luisdavidnazareth@gmail.com>
2024-11-11 23:41:19 -0300

    actualizacion y creacion de controller

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import "dotenv/config.js"
 import "./config/database.js"
-
-
+import cors from 'cors'
+import morgan from "morgan";
+import indexRouter from './router/index.js'
 
 const server = express()
 
@@ -10,9 +11,13 @@ const PORT = process.env.PORT || 8080
 
 const ready = () => console.log("server ready in port = "+PORT);
 
-server.get('/',(req,res)=>{
-    res.send('Hola mundo en express')
-})
+server.use(express.json())
+server.use(express.urlencoded({extended:true}))
+server.use(cors())
+server.use(morgan('dev'))
+
+
+server.use('/api',indexRouter)
 
 server.listen(PORT,ready)
 
