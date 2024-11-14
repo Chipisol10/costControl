d366749911429204cf9b7f6af16ea1fd6a01fc9a
Luis G <luisdavidnazareth@gmail.com>
2024-11-14 16:00:51 -0300

    modifico middleware validator y shemas user, properties

--- schemas/properties/createProperties.js
+++ schemas/properties/createProperties.js
@@ -3,7 +3,8 @@ import joi from "joi-oid";
 const schema = joi.object({
     name: joi.string().required().alphanum().messages({
         'string.base':"Please only letters",
-        'string.alphanum':'Please do not use special characters'
+        'string.alphanum':'Please do not use special characters',
+        'string.empty':'Name Required'
     }),
     type:joi.string().required(),
     value:joi.number().required().positive().message({
